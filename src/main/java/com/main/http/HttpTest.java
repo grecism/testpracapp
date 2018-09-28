8a8da5a6759165084e58ea5e50dc03fe7fad6089
grecism <grecism@163.com>
2018-09-28 10:53:00 +0800

    thread

---
+++ src/main/java/com/main/http/HttpTest.java
@@ -0,0 +1,9 @@
+package com.main.http;
+
+/**
+ * @author lulu.zou
+ * @version 2018/5/11
+ * @since 2018/5/11
+ */
+public class HttpTest {
+}
---
+++ src/main/java/com/main/http/httpclient/hccommons/HcCommons_01_Test.java
@@ -0,0 +1,9 @@
+package com.main.http.httpclient.hccommons;
+
+/**
+ * @author lulu.zou
+ * @version 2018/5/11
+ * @since 2018/5/11
+ */
+public class HcCommons_01_Test {
+}
